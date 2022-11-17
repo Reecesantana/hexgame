368fd5a57cce2dc155b4a02c7b9688b1d843e72f
Reece <Reecesantana@gmail.com>
2022-11-17 17:07:15 -0500

    purge tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  purge: [],
+  purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}',
+	],
   theme: {
     extend: {},
   },
